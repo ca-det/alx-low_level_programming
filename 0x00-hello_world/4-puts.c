e63668a57ca7fddba46fc59b3838c139d2e85085
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-16 12:42:51 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-a programm to print a line using function puts
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
