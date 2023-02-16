55a8c4474f6e6c14ceedfabfab31d4ba73fdf661
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-16 12:58:58 -0800

    sizey

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(e));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
