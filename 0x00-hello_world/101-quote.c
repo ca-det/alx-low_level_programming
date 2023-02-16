0d5d7c4ed9942f45ea4f3354c666ce12b0ac16a1
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-16 13:29:37 -0800

    quotw

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stiod.h>
+#include <unistd.h>
+/**
+ * main- programm to print line without certain functions
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
