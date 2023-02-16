6df5016e44e4cb23029ce406b2b04d7bf87629fc
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-16 09:35:13 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - A programm that prints a line using printf function
- *
- * Retun: 0 (Success)
+ * main- A programm that prints a line using printf function to a new line
+ * Description: A programm that prints a line using printf function
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	pritf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
