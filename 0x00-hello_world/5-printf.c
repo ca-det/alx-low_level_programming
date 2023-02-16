3f9aff51176ce67eadeb887d5b772c8218b051ee
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-16 09:15:38 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a line using printf function
+ *
+ * Retun: 0 (Success)
+ */
+
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
