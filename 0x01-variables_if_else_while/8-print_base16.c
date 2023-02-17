47cacfbe005b3290f1851f9bc040a64cc348e091
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-17 14:41:25 -0800

    test 2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main- base 16 numbers
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char new;
+int num;
+
+for (num = 0 ; num < 10 ; num++)
+{
+putchar((num % 10) + '0');
+}
+for (new = 'a' ; new <= 'f' ; new++)
+{
+putchar (new);
+}
+putchar('\n');
+return (0);
+}
