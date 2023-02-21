982451520f171f381d5e6cc63de3d1e50d99b1c8
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-21 15:09:52 +0300

    print letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet- printing all the letters
+*/
+void print_alphabet(void)
+{
+char a;
+
+for (a = 'a' ; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+/*return (0);*/
+
+}
