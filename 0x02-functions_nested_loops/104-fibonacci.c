de3269deb7ffa2dd175366f814f620ba6c8ca588
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-22 17:29:24 +0300

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -26,7 +26,6 @@ for (count = 93 ; count < 99 ; count++)
 {
 half1 = half11 + half21;
 half2 = half12 + half22;
-}
 if (half12 + half22 > 9999999999)
 {
 half1 += 1;
@@ -35,11 +34,14 @@ half2 %= 10000000000;
 printf("%lu%lu", half1, half2);
 if (count != 98)
 {
+printf(", ");
 half11 = half21;
 half12 = half22;
 half21 = half1;
 half22 = half2;
 }
+}
 printf("\n");
 return (0);
+
 }
