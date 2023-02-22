c003d64dcbc2c251c827e33f786034b1f79680e8
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-22 05:57:02 +0300

    trial up

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- priint all to 98
+ * @n: parameter
+*/
+void print_to_98(int n)
+{
+int num1;
+if (n <= 98)
+{
+for (num1 = n ; num1 <= 98 ; num1++)
+{
+if (num1 != 98)
+{
+printf("%d, ", num1);
+}
+if (num1 == 98)
+{
+printf("%d\n", num1);
+}
+}
+/*first end*/
+}
+else if (n == 98)
+{
+printf("%d\n", n);
+}
+/*second end*/
+else if (n >= 98)
+{
+for (num1 = n ; num1 >= 98 ; num1--)
+{
+if (num1 != 98)
+printf("%d, ", num1);
+}
+if (num1 == 98)
+{
+printf("%d\n", num1);
+}
+}
+/*third end*/
+}
