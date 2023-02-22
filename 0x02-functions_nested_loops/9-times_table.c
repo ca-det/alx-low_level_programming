2eff00e48d3815cc95e8022430e8fbcbd63094d8
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-22 10:40:18 +0300

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *times_table- printing table 9
+ *
+*/
+void times_table(void)
+{
+int i, j;
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+printf("%d", i * j);
+if (j != 9)
+{
+printf(", ");
+/*printf("%d", i*j);*/
+}
+}
+puts(" ");
+}
+
+}
