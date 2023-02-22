62a287388f2a9fdf0f7b9dd71643464ec65bd5c2
EphesiansCadet <ephesiansokoth@gmail.com>
2023-02-22 18:28:30 +0300

    test 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,24 +1,15 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdio.h>
 
- /**
-  *  positive_or_negative- print absolution
-  * Return: Always 0
-  */
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
-void positive_or_negative(int i)
+int main(void)
 {
-	if (i < 0)
-	{
-		printf("%d is negative\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is positive\n", i);
-	}
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
 }
