75eff4ba59a3e821a011a357a40593645807b415
EphesiansCadet <ephesiansokoth@gmail.com>
2023-03-06 15:09:31 +0300

    check 3

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,7 @@ char *_strstr(char *haystack, char *needle)
 		char *o = haystack;
 		char *p = needle;
 
-		while ( *o == *p && *p != '\0')
+		while (*o == *p && *p != '\0')
 		{
 			o++;
 			p++;
