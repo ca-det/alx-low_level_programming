6a06fff85200bf52c43c320960e60236791c6ace
Ephesians Lewis <100203395+ca-det@users.noreply.github.com>
2023-08-17 19:01:13 +0300

    up

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
+ * print_dlistint - prints all the elements
+ *
  *
  * @h: head of the list
  * Return: the number of nodes
