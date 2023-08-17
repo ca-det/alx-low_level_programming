ada5bfcd3a07c83f358dea935d784e5d1034f914
Ephesians Lewis <100203395+ca-det@users.noreply.github.com>
2023-08-17 19:01:51 +0300

    up

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,7 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
+ * dlistint_len - returns the number of elements
  * @h: head of the list
  * Return: the number of nodes
  */
